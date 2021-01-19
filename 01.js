02d69af5e54b201a2834e951f0c0e233c5870300
kazukimizu <zm18268@josai.ac.jp>
2021-01-19 02:46:48 +0000

    0119

--- 01.js
+++ 01.js
@@ -2,6 +2,6 @@ function nannnenn01() {alert('今年は,' + document.getElementById('when01').in
 function seireki01() {document.getElementById('when01').innerHTML = '西暦 2021年';}
 function wareki01() {document.getElementById('when01').innerHTML = '和暦 令和3年';}
 
-function sayhello68() {alert('Hello, ' + document.getElementById('who68').innerHTML + '!');}
-function taro68() {document.getElementById('who68').innerHTML = '太郎';}
-function hanako68() {document.getElementById('who68').innerHTML = '花子';}
+function sayhello68() {alert('今年は, ' + document.getElementById('who68').innerHTML + '!');}
+function taro68() {document.getElementById('who68').innerHTML = '西暦 2021年';}
+function hanako68() {document.getElementById('who68').innerHTML = '和暦 令和3年';}
