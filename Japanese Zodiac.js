20f0ee9e4d13df5d42244ecb676ed8fba8f124b8
kazukimizu <zm18268@josai.ac.jp>
2021-01-26 02:16:04 +0000

    0126

---
+++ Japanese Zodiac.js
@@ -0,0 +1,3 @@
+function txtchange1() {document.getElementById('txt1').innerHTML = '丑年！';}
+function imgchange1() {document.getElementById('logo').src = './image/cow.png';}
+function imgback1() {document.getElementById('logo').src = './image/Japanese Zodiac.png';}
